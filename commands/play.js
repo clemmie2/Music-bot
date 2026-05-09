09f6f43b01e83b2ed184e97b1519ce87c27c0d2f
clemmie2 <clemmons066@gmail.com>
2026-05-09 05:14:53 +0000

    add play command to handle music playback using Lavalink

---
+++ commands/play.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: "play",
+    description: "Play music Using Lavalink",
+
+    async execute(interaction) {
+        const query = interaction.options.getString("query");
+
+        const player= await client.shoukaku.joinVoiceChannel({
+            guildid: interaction.guild.id,
+            channelid: interaction.member.voice.channel.id,
+            shardid: interaction.guild.shardid
+        }):
+
+        const result = await client.shoukaku.rest.resolve(query);
+
+        if(!result?.tracks?.length)
+             return interaction.reply("No results found.");
+
+        player.play(result.tracks[0]);
+
+        player.playTrack({ track: track track.encoded });
+
+        interaction.reply('Now playing: **${track.info.title}**');
+}
+};
