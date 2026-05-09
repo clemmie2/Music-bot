83fb8382422993a9304d244b4c33ec2fc9be2caa
clemmie2 <clemmons066@gmail.com>
2026-05-09 07:57:57 +0000

    feat: add command to toggle 24/7 mode for the current guild

---
+++ commands/247.js
@@ -0,0 +1,16 @@
+const manager = require("../structures/PlayerManager");
+
+module.exports = {
+    name: "247",
+    description: "Toggles 24/7 mode for the current guild."
+
+    async execute(interaction) {
+        const state = manager.247(interaction.guild.id) || false;
+
+        manager.247.set(interaction.guild.id, !state);
+
+        interaction.reply(interaction.guild.id, !state);
+
+        interaction.reply(`24/7 mode: ${!state ? "ON" : "OFF"}`);
+    }
+};
