ad79ccf6a2546c91f6dd024582f99e97f157c156
clemmie2 <clemmons066@gmail.com>
2026-05-09 08:01:42 +0000

    fix: remove description from 247 command module export

--- commands/247.js
+++ commands/247.js
@@ -2,7 +2,6 @@ const manager = require("../structures/PlayerManager");
 
 module.exports = {
     name: "247",
-    description: "Toggles 24/7 mode for the current guild."
 
     async execute(interaction) {
         const state = manager.247.get(interaction.guild.id) || false;
