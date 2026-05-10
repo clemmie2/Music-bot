c217e263442637441457c7c09391cdd3bf308d58
clemmie2 <clemmons066@gmail.com>
2026-05-10 09:58:51 +0800

    Update lavalink configuration details

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-      lavalink: {
-      name: "Main",
-      url: "lava-v4.millohost.my.id:443",
-      auth: "https://discord.gg/mjS5J2K3ep",
-      secure: true
-    }
+  lavalink: {
+    name: "MainNode",
+    url: "lava-v4.millohost.my.id:443",
+    auth: "YOUR_LAVALINK_PASSWORD",
+    secure: true
+  }
 };
